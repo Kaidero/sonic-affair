ebf8170db0e4b2a0cb6247dc8a9303efbf4d1251
Kai de Rochemont <kaiderochemont@hey.com>
2023-04-21 22:57:37 -0600

    change site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kaidero.github.io',
+  site: 'https://www.asonicaffair.com',
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*"
     }
   },
-  base: '/sonic-affair',
+  base: '/',
 })
